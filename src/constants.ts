be68a272bba81203f3263783be5751a642d8ea73
mcchas <mcchas@machine.local>
2024-06-15 23:08:57 +1000

    add constants.ts, reuse http sessions

---
+++ src/constants.ts
@@ -0,0 +1,4 @@
+export const userPoolId = "ap-southeast-2_WSJBAl6hB";
+export const userPoolClientId = "56bpopumtgnheqfchknuchdhvt";
+export const clientSecret = "1l82hrk9jtu3irer8iouf4sgrof6f43j91i4dgrc9i3hcna2ttmu"; // this not-so-secret has intentionally been included
+export const endpoint = "https://api-s.gainsboroughhardware.cloud/v0"; 
